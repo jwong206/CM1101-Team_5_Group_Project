77ad008380a513e90af7f4c51189d90bc7d6d152
Chinglebells <charliemichaelbennett@gmail.com>
2016-10-14 11:00:02 +0100

    Actually implemented starting room.

--- Actual Game Files/player.py
+++ Actual Game Files/player.py
@@ -4,4 +4,4 @@ from map import rooms
 inventory = [item_id, item_laptop, item_money]
 
 # Start game at the reception
-current_room = rooms["-enter starting room-"]
+current_room = rooms["Enterprise"]
