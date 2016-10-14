dd963ee0df75b4cfc27b048ccca37c463b78f1c1
Chinglebells <charliemichaelbennett@gmail.com>
2016-10-14 10:46:25 +0100

    Moved into game folder.

---
+++ Actual Game Files/items.py
@@ -0,0 +1,51 @@
+item_id = {
+    "id": "id",
+
+    "name": "id card",
+
+    "description":
+    """You new shiny student ID card. Expires 1 June 2017.
+You wonder why they have printed a suicide hotline number on it?..."""
+}
+
+item_laptop = {
+    "id": "laptop",
+
+    "name": "laptop",
+
+    "description":
+    "It has seen better days. At least it has a WiFi card!"
+}
+
+item_money = {
+    "id": "money",
+
+    "name": "money",
+
+    "description":
+    "This wad of cash is barely enough to pay your tuition fees."
+}
+
+item_biscuits = {
+    "id": "biscuits",
+
+    "name": "a pack of biscuits",
+
+    "description": "A pack of biscuits."
+}
+
+item_pen = {
+    "id": "pen",
+    
+    "name": "a pen",
+
+    "description": "A basic ballpoint pen."
+}
+
+item_handbook = {
+    "id": "handbook",
+    
+    "name": "a student handbook",
+
+    "description": "This student handbook explains everything. Seriously."
+}
